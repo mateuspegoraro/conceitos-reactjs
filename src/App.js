0498f6c09d31e1dbd8961d24602d74e070ec68d9
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-06-20 21:25:44 -0300

    Criação e remoção de repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      //console.log(response);
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+        "title" : "Conceitos React JS",
+        "url" : "https://github.com/mateuspegoraro/forum-spring",
+        "techs" : ["spring", "jwt", "java"]
+      }
+
+    const response = await api.post('repositories', repository);
+    setRepositories([ ... repositories, response.data]);
+
+    console.log(repositories);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+    if(repositoryIndex >= 0 && (response.status >= 200 && response.status < 300)){
+      repositories.splice(repositoryIndex, 1);
+      setRepositories([... repositories])
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => 
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
